804637c11c7407edddf15ef14ca78a4a74215409
Sushiy <julivizi@yahoo.de>
2016-12-30 02:36:22 +0100

    Fixed MoveToAttack Crash

--- Assets/Scripts/Character/Movement.cs
+++ Assets/Scripts/Character/Movement.cs
@@ -14,7 +14,7 @@ public class Movement : MonoBehaviourTrans
     public float m_fTurningSpeed = 120.0f; //120.0f is the standard turningSpeed
 
     Vector3 m_v3Target; //The last target we wanted to reach
-    float fAttackRangeSampleRate = 0.1f;
+    float m_fAttackRangeSampleRate = 0.1f;      //The Rate at which we check along the path to find a point in combat range
 
     // Use this for initialization
     void Awake() 
@@ -82,13 +82,40 @@ public class Movement : MonoBehaviourTrans
 
         Agent.SamplePathPosition(Agent.areaMask, Agent.remainingDistance, out navmeshhit); //sample the last point of the path
         //Go through the path from end and sample each point to check if it is in range of the enemy
-        for (float fRange = Agent.remainingDistance - fAttackRangeSampleRate; (navmeshhit.position - _v3Target).magnitude < fActualRange; fRange -= fAttackRangeSampleRate)
+        int iCornerCount = Agent.path.corners.Length;
+        int iLastIndexInRange = iCornerCount - 1;
+        Vector3 v3BestSample = Agent.path.corners[iCornerCount - 1];
+        for (int i = iCornerCount - 2; i > 0; i--)
         {
-            //Sample each point until the distance to the target has been maxed out
-            Agent.SamplePathPosition(Agent.areaMask, fRange, out navmeshhit);
+            if(Vector3.Distance(Agent.path.corners[i], _v3Target) < fActualRange)
+            {
+                iLastIndexInRange = i;
+            }
+            else
+            {
+                break;
+            }
+        }
+        v3BestSample = Agent.path.corners[iLastIndexInRange];
+        //If the Index we found was not the first point on our path, the point with the exact distance of our CombatRange in the segment between lastIndexInRange and lastIndexInRange+1
+        if (iLastIndexInRange != 1)
+        {
+            Vector3 v3Segment = Agent.path.corners[iLastIndexInRange] - Agent.path.corners[iLastIndexInRange - 1];
+            for (float f = 0; f < 1.0f; f -= m_fAttackRangeSampleRate)
+            {
+                Vector3 v3Sample = Agent.path.corners[iLastIndexInRange] + v3Segment * f;
+                if(Vector3.Distance(_v3Target, v3Sample) <= fActualRange)
+                {
+                    v3BestSample = v3Sample;
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
 
-        MoveTowards(navmeshhit.position);
+        MoveTowards(v3BestSample);
     }
 
     //Rotates the agent towards the given Target
